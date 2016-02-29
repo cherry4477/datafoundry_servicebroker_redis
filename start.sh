31ac0144702689c65902333bd24f6119dd1792ab
root <root@ip-172-31-24-14.cn-north-1.compute.internal>
2016-02-29 17:26:28 +0800

    修改启动脚本，增加动态库软链接

--- start.sh
+++ start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ ! -f /usr/lib64/libcurl.so ]
+then
+ 	echo "creating ln libcurl.so..."
+	ln -s  /usr/lib64/libcurl.so.4 /usr/lib64/libcurl.so
+fi
+
 ./serviceBrokerRedis -c ./config/serviceBroker.conf -n start
 
 #nohup ./serviceBrokerRedis -c ./config/serviceBroker.conf -n start 2>&1 &
